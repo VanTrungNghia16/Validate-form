f2f7940c830024bce1a7948f45b1df5585504e2f
Your Name <you@example.com>
2022-01-25 22:51:17 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,94 @@
+var username = document.querySelector('#username');
+var email = document.querySelector('#email');
+var password = document.querySelector('#password');
+var confirmPassword = document.querySelector('#confirm-password');
+var form = document.querySelector('form');
+
+function showError(input, message) {
+    let parent = input.parentElement;
+    let small = parent.querySelector('small');
+
+    parent.classList.add('error');
+    small.innerText = message;
+    return true;
+}
+
+function showSuccess(input) {
+    let parent = input.parentElement;
+    let small = parent.querySelector('small');
+
+    parent.classList.remove('error');
+    small.innerText = '';
+    return true;
+}
+
+function checkEmptyError(listInput) {
+    let isEmptyError = false; // Toàn bộ form không có lỗi
+    listInput.forEach((input) => {
+        if(!input.value) {
+            isEmptyError = true;
+            showError(input, 'Khong Duoc De Trong');
+        }
+        else{
+            showSuccess(input);
+        }
+    })
+    return isEmptyError;
+}
+
+function checkEmailError(input) {
+    const regexEmail =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    input.value = input.value.trim();
+
+  let isEmailError = false; // Email không có lỗi
+  if(regexEmail.test(input.value)) {
+      showSuccess(input);
+      isEmailError = true;
+  }
+  else{
+      showError(input, 'Email Invalid')
+     
+  }
+  return isEmailError;
+}
+
+function checkLengthError(input, min, max) {
+    input.value = input.value.trim();
+    if(input.value.length < min) {
+        showError(input, `Phai co it nhat ${min} ky tu`);
+        return true;
+    }
+    if(input.value.length > max) {
+        showError(input, `khong duoc vuot qua ${max} ky tu`);
+        return true;
+    }
+
+    return false;
+}
+
+function checkMatchPasswordError(passwordInput, cfPasswordInput) {
+    if(passwordInput.value !== cfPasswordInput.value) {
+        showError(cfPasswordInput, 'Mat khau khong trung khop');
+        return true;
+    }
+    return false;
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let isEmptyError = checkEmptyError([username, email, password, confirmPassword]);
+
+    let isEmailError = checkEmailError(email);
+    let isUsernameLengthError = checkLengthError(username, 3, 10);
+    let isPasswordLengthError = checkLengthError(password, 3, 10);
+    let isMatchError = checkMatchPasswordError(password, confirmPassword);
+
+    if(isEmptyError || isEmailError || isUsernameLengthError || isPasswordLengthError || isMatchError) {
+        // do not eveything
+    }
+    else{
+        // logic, Call APi
+    }
+})
